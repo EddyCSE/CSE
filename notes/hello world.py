5e222e1cd07d7b67d213c32ef6ff252855549aa3
EddyCSE <44374148+EddyCSE@users.noreply.github.com>
2018-10-23 09:46:19 -0700

    Math/Comments Stuff
    Doing math and comments

--- notes/hello world.py
+++ notes/hello world.py
@@ -1,1 +1,28 @@
 print("hello world")
+
+# This is a comment. This has no effect on the code
+# but this does allow me to do things. I can:
+# 1. Make notes to myself.
+# 2. Comment pieces of code that do not work.
+# 3. Make by my code easier to read.
+
+print("Look at what happens here. Is there any space?")
+print()
+print()
+print("There should be a couple of blank lines here.")
+
+# Math
+print(3 + 5)
+print(5 - 2)
+print(3 + 4)
+print(6 / 2)
+
+print("Figure this out...")
+print(6 // 2)
+print(5 // 2)
+print(9 // 4)
+
+print("Here is another one...")
+print(6 % 2)
+print(5 % 2)
+print(11 % 4)  # Modulus (Remainder)
