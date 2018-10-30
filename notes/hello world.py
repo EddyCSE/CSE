0e5ca2523d19f0bf8db789dfe5ec7ec5d6637fed
EddyCSE <44374148+EddyCSE@users.noreply.github.com>
2018-10-30 09:46:06 -0700

    Added Loops 10/30/18

--- notes/hello world.py
+++ notes/hello world.py
@@ -1,3 +1,4 @@
+"""
 print("hello world")
 
 # This is a comment. This has no effect on the code
@@ -53,3 +54,52 @@ print("I have a car called %s. It is a %s." % (car_name, car_type))
 real_age = int(input("How old are you again?"))
 hidden_age = real_age + 5
 print("This is your real age: %d" % hidden_age)
+"""
+
+"""
+This is a multi-line comment
+Anything between the "s is not a run.
+"""
+
+
+# Functions
+def say_it():
+    print("Hello World!")
+
+
+say_it()
+say_it()
+say_it()
+
+
+# f(x) = 2x + 3
+def f(x):
+    print(2*x + 3)
+
+
+f(1)
+f(5)
+f(5000)
+
+
+# Distance Formula
+def distance(x1, y1, x2, y2):
+    dist = ((x2-x1)**2 + (y2-y1)**2)**(1/2)
+    print(dist)
+
+
+distance(0, 0, 3, 4)
+distance(0, 0, 5, 12)
+
+
+# Loops
+for i in range(5):  # This gives the numbers 0 through 4
+    say_it()
+
+for i in range(10):
+    print(i + 1)
+
+for i in range(5):
+    f(i)
+
+# While loops
