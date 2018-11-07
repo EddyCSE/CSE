53c45a8953918a1ff226bec844f2506b3b733aa3
EddyCSE <44374148+EddyCSE@users.noreply.github.com>
2018-11-07 09:45:47 -0800

    11/7/18 guess game finished and lucky 7s start

--- notes/Eduardo Sanchez - Guess Game.py
+++ notes/Eduardo Sanchez - Guess Game.py
@@ -1,17 +1,19 @@
 import random
-(random.randint(0, 10))
-guesses_taken = 5
+r = (random.randint(0, 10))
+guesses_left = 5
+playing = True
 
 print("Guess a number from 1 to 10")
-Guess1 = input("1st guess=")
-Guess1 = int(Guess1)
 
-while guesses_taken < 5:
-    if Guess1 > random.randint:
+while guesses_left > 0 and playing:
+    guess = int(input("Guess="))
+    if guess > r:
         print("Lower")
-    elif Guess1 < random.randint:
+        guesses_left -= 1
+    elif guess < r:
         print("Greater")
+        guesses_left -= 1
     else:
-        print("Correct")
 
-print(random.randint)
+        print("Correct!!!")
+        playing = False
---
+++ notes/Eduardo Sanchez - Lucky 7s.py
@@ -0,0 +1,7 @@
+import random
+money = 15
+playing = True
+
+while money > 0 and playing:
+    money -= 1
+    (random.randint(1, 6))(random.randint(1, 6))
