4254d3cd8a5e69249aa432282c02cf2df7e8aee1
EddyCSE <44374148+EddyCSE@users.noreply.github.com>
2018-11-08 09:46:13 -0800

    11/8/18 finished lucky 7s

--- notes/Eduardo Sanchez - Lucky 7s.py
+++ notes/Eduardo Sanchez - Lucky 7s.py
@@ -1,7 +1,16 @@
 import random
 money = 15
+rounds = 0
 playing = True
 
 while money > 0 and playing:
     money -= 1
-    (random.randint(1, 6))(random.randint(1, 6))
+    rounds += 1
+    if random.randint(1, 6) + random.randint(1, 6) == 7:
+        money += 5
+    else:
+        playing = True
+        if money == 0:
+            print("You've played,")
+            print(rounds)
+            print("rounds.")
