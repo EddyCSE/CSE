369a49e58706144785f4cdd63b19eb8e3d46e21a
EddyCSE <44374148+EddyCSE@users.noreply.github.com>
2018-11-14 09:38:54 -0800

    11/14/18 lucky 7s
    extension done

--- notes/Eduardo Sanchez - Lucky 7s.py
+++ notes/Eduardo Sanchez - Lucky 7s.py
@@ -2,15 +2,17 @@ import random
 money = 15
 rounds = 0
 playing = True
+max_money = 15
 
 while money > 0 and playing:
     money -= 1
     rounds += 1
     if random.randint(1, 6) + random.randint(1, 6) == 7:
         money += 5
+        if money >= max_money:
+            max_money += 5
     else:
         playing = True
         if money == 0:
-            print("You've played,")
-            print(rounds)
-            print("rounds.")
+            print("You've played %s rounds." % rounds)
+            print("You should've stopped at %s dollars." % max_money)
