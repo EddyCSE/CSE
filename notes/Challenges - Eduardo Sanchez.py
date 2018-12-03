9007fffc4d846bd23a7bd140f1f0c3abf7e0278d
EddyCSE <44374148+EddyCSE@users.noreply.github.com>
2018-12-03 09:45:04 -0800

    12/3/18
    challenges

---
+++ notes/Challenges - Eduardo Sanchez.py
@@ -0,0 +1,21 @@
+'''
+# Challenge 1
+
+
+def challenge1(first_name, last_name):
+    print(last_name, first_name)
+
+
+challenge1("Eduardo", "Sanchez")
+'''
+# Challenge 2
+
+
+def challenge2(random_number):
+    if random_number // 2:
+        print("even number")
+    else: 
+        print("odd number")
+
+
+challenge2(7)
--- notes/hello world.py
+++ notes/hello world.py
@@ -249,3 +249,13 @@ for character in list1:
 
 # Turn a list into a string
 print("".join(list1))
+
+# Function Notes
+# a**2 + b**2 = c**2
+
+
+def pythagorean(a, b):
+    return (a**2 + b**2)**(1/2)
+
+
+print(pythagorean(3, 4))
