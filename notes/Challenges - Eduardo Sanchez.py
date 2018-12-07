931d82b24023450548257b181620e6adc19d9fe4
EddyCSE <44374148+EddyCSE@users.noreply.github.com>
2018-12-07 10:41:04 -0800

    12/7/18

--- notes/Challenges - Eduardo Sanchez.py
+++ notes/Challenges - Eduardo Sanchez.py
@@ -31,3 +31,18 @@ print(challenge5(10))
 # Challenge 6
 
 
+def challenge6(r):
+    return (4/3 * 3.14) * r ** 3
+
+
+print(challenge6(3))
+
+
+# Challenge 8
+
+
+def challenge7(n):
+    return n + (n*n) + (n*n*n)
+
+
+print(challenge7(10))
