f5b7dd6450f2d954e3fa3e2fd5c8347b85e9f4cf
EddyCSE <44374148+EddyCSE@users.noreply.github.com>
2019-01-08 09:44:56 -0800

    1/8/19
    Notes

---
+++ Semester 2 Notes.py
@@ -0,0 +1,25 @@
+"""
+print("Hello World")
+
+# single-line comment
+
+Cars = 5
+Driving = True
+
+print("I have %d cars." % Cars)
+print("I have " + str(Cars) + " cars.")
+
+Age = input("How old are you?")
+print(Age + "? lmao, weak.")
+"""
+
+Colors = ["Red", "Orange", "Yellow", "Green", "Blue"]
+Colors.append("Purple")
+
+print(Colors)
+Colors.pop(0)
+print(Colors)
+
+print(Colors[2])
+
+print(len(Colors))
