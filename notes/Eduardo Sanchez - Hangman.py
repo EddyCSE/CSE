b5452a47a7f6d339fa755edc644dcffe21c4ae06
EddyCSE <44374148+EddyCSE@users.noreply.github.com>
2019-01-17 09:44:51 -0800

    1/17/19

--- notes/Eduardo Sanchez - Hangman.py
+++ notes/Eduardo Sanchez - Hangman.py
@@ -1,17 +1,20 @@
 import random
 import string
-
-print("Guess out my word...")
-
+guesses = 6
+output = []
+AllLetters = string.ascii_letters
 words = ["thigh", "computer", "mouse", "chair", "shirt", "o'clock", "concatenation", "pajama", "soccer", "sweater",
          "goldfish", "human", "haphazard", "wildebeest", "pixel", "Wiebe"]
 
+print("Guess out my word...")
 randword = (random.choice(words))
-randword = '-' * len(randword)
 wordlist = []
-wordlist.append(randword)
-print(wordlist)
+wordlist = list(randword)
+length = len(randword)
 
-guess1 = input("Guess a letter")
 
-if guess1
+output = len(list(randword))
+print(output)
+
+
+
