77a751ee84bee14c33fc64d710347255398b759c
EddyCSE <44374148+EddyCSE@users.noreply.github.com>
2019-01-22 09:45:55 -0800

    1/22/19

--- notes/Eduardo Sanchez - Hangman.py
+++ notes/Eduardo Sanchez - Hangman.py
@@ -6,18 +6,37 @@ AllLetters = string.ascii_letters
 words = ["thigh", "computer", "mouse", "chair", "shirt", "o'clock", "concatenation", "pajama", "soccer", "sweater",
          "goldfish", "human", "haphazard", "wildebeest", "pixel", "Wiebe"]
 
-print("Guess out my word...")
 randword = (random.choice(words))
-wordlist = []
-output = randword
 wordlist = list(randword)
 length = len(randword)
 
-print(output)
-print(randword)
-print(wordlist)
-
+# Takes the randword into blanks
 for i in range(length):
-    randword = wordlist.append = "*"
+    output.append("_ ")
+print("".join(output))
 
-print(wordlist)
+# Takes the guess and checks it in the word list
+# The "\n" is a full line space
+while guesses > 0 and len(wordlist) > 0:
+    guess = input("Guess out my word, letter = ")
+    print("\n" * 20)
+    if guess in randword:
+        print("Nice, that's a letter.")
+        print(guesses)
+        for i in range(len(randword)):
+            if guess in wordlist:
+                wordlist.pop(i)
+        for i in range(len(randword)):
+            if randword[i] == guess:
+                output.pop(i)
+                output.insert(i, guess)
+        print("".join(output))
+    else:
+        print("Wrong, guess again.")
+        guesses = (guesses - 1)
+        print(guesses)
+    if guesses <= 0:
+        print("You ran out of guesses, you failed, he's dead, shame on you")
+    if len(wordlist) == 0:
+        print("Congrats, you won!")
+        print("The word was %s" % randword)
--- notes/testing page.py
+++ notes/testing page.py
@@ -3,3 +3,12 @@ print(list(string.ascii_letters))
 print(string.digits)
 print(string.punctuation)
 print(string.whitespace)
+
+print(output)
+print(randword)
+print(wordlist)
+
+for i in range(length):
+    randword = wordlist.append = "*"
+
+print(wordlist)
