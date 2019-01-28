c978efcf1b6bca7d845123afc66d56244e6e1531
EddyCSE <44374148+EddyCSE@users.noreply.github.com>
2019-01-28 09:44:52 -0800

    1/28/19

--- notes/Eduardo Sanchez - Hangman.py
+++ notes/Eduardo Sanchez - Hangman.py
@@ -1,16 +1,17 @@
 import random
 import string
-guesses = 6
-output = []
+
 AllLetters = string.ascii_letters
 words = ["thigh", "computer", "mouse", "chair", "shirt", "o'clock", "concatenation", "pajama", "soccer", "sweater",
          "goldfish", "human", "haphazard", "wildebeest", "pixel", "Wiebe"]
 
-print("".join(output))
-
+guesses = 6
+output = []
 randword = (random.choice(words))
 wordlist = list(randword)
 length = len(randword)
+
+print("".join(output))
 
 # Takes the randword into blanks
 for i in range(length):
@@ -21,39 +22,38 @@ print(randword)
 # Takes the guess and checks it in the word list
 # The "\n" is a full line space
 
-while guesses > 0 and length > 0:
+while guesses > 0 and len(wordlist) > 0:
     guess = input("Guess out my word, letter = ")
     print("\n" * 20)
     if guess in randword:
         print("Nice, that's a letter.")
         print(guesses)
-        for i in range(length):  # Testing lines from this one
+        for i in range(length):
             if guess in wordlist:
                 wordlist.pop(i)
-                wordlist.insert(i)
         for i in range(length):
             if randword[i] == guess:
                 output.pop(i)
-                output.insert(i, guess)  # To this one
+                output.insert(i, guess)
         print("".join(output))
-        if wordlist == 0:  # Checks if you won
+        if len(wordlist) == 0:  # Checks if you won
             print("\n" * 10)
             print("Congrats, you won!")
             print("The word was %s" % randword)
+            quit()
     else:
         print("Wrong, guess again.")
         guesses = (guesses - 1)
         print(guesses)
         print("".join(output))
-    if guesses <= 0:
-        print("\n" * 10)
-        print("You ran out of guesses, you failed, he's dead, shame on you")
-    if len(wordlist) == 0:  # Checks if you won
-        print("\n" * 10)
-        print("Congrats, you won!")
-        print("The word was %s" % randword)
+        if guesses <= 0:
+            print("\n" * 10)
+            print("You ran out of guesses, you failed, he's dead, shame on you")
+        if len(wordlist) == 0:  # Checks if you won
+            print("\n" * 10)
+            print("Congrats, you won!")
+            print("The word was %s" % randword)
+            # quit()
 
 print(wordlist)
-print(randword)
-print(length)
 print(output)
