a83eb207072e035ef773571001da5bd2b55da092
EddyCSE <44374148+EddyCSE@users.noreply.github.com>
2019-01-29 09:45:31 -0800

    1/29/19

--- Semester 2 Notes.py
+++ Semester 2 Notes.py
@@ -11,7 +11,7 @@ print("I have " + str(Cars) + " cars.")
 
 Age = input("How old are you?")
 print(Age + "? lmao, weak.")
-"""
+
 
 Colors = ["Red", "Orange", "Yellow", "Green", "Blue"]
 Colors.append("Purple")
@@ -23,3 +23,83 @@ print(Colors)
 print(Colors[2])
 
 print(len(Colors))
+
+"""
+# Dictionary Notes
+
+states = {
+    "CA": "California",
+    "FL": "Florida",
+    "AK": "Alaska",
+    "GA": "Georgia"
+}
+
+print(states["CA"])
+print(states["AK"])
+
+nested_dictionary = {
+    "CA": {
+        "NAME": "California",
+        "POPULATION": 39500000  # 39,500,000
+    },
+    "FL": {
+        "NAME": "Florida",
+        "POPULATION": 21300000  # 21,300,000
+    },
+    "AK": {
+        "NAME": "Alaska",
+        "POPULATION": 737000    # 737,000
+    },
+    "GA": {
+        "NAME": "Georgia",
+        "POPULATION": 10500000  # 10,500,000
+    }
+}
+
+print(nested_dictionary["GA"]["POPULATION"])
+print(nested_dictionary["FL"]["NAME"])
+
+georgia = (nested_dictionary["GA"])
+print(georgia)
+
+complex_dictionary = {
+    "CA": {
+        "NAME": "California",
+        "POPULATION": 39500000,
+        "CITIES": [
+            "Fresno",
+            "San Fransisco",
+            "Los Angeles"
+        ]
+    },
+    "FL": {
+        "NAME": "Florida",
+        "POPULATION": 21300000,
+        "CITIES": [
+            "Miami",
+            "Orlando",
+            "Tampa",
+            "Jacksonville"
+        ]
+    },
+    "AK": {
+        "NAME": "Alaska",
+        "POPULATION": 737000,
+        "CITIES": [
+            "Anchorage",
+            "Fairbanks",
+            "Juneau"
+        ]
+    },
+    "GA": {
+        "NAME": "Georgia",
+        "POPULATION": 10500000,
+        "CITIES": [
+            "Atlanta",
+            "Savannah",
+            "Augusta"
+        ]
+    }
+}
+
+print(complex_dictionary["AK"]["CITIES"][0])
