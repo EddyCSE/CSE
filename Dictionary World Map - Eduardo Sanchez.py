ab3bba32bcd4ca247e038077325c76cf4506b00d
EddyCSE <44374148+EddyCSE@users.noreply.github.com>
2019-01-31 09:44:38 -0800

    1/31/19

--- Dictionary World Map - Eduardo Sanchez.py
+++ Dictionary World Map - Eduardo Sanchez.py
@@ -19,9 +19,20 @@ world_map = {
 # Controller   node = area
 playing = True
 current_node = world_map['R19A']
+directions = ['NORTH', 'SOUTH', 'EAST', 'WEST', 'UP', 'DOWN']
+
 while playing:
     print(current_node['NAME'])
     print(current_node['DESCRIPTION'])
     command = input(">_")
     if command.lower() in ['q', 'quit', 'exit']:
         playing = False
+    elif command.upper() in directions:
+        try:
+            room_name = current_node['PATHS'][command.upper()]
+            current_node = world_map[room_name]
+        except KeyError:
+            print("I can't go that way")
+
+    else:
+        print("Command Not Found")
--- notes/testing page.py
+++ notes/testing page.py
@@ -4,11 +4,10 @@ print(string.digits)
 print(string.punctuation)
 print(string.whitespace)
 
-print(output)
-print(randword)
-print(wordlist)
 
-for i in range(length):
-    randword = wordlist.append = "*"
-
-print(wordlist)
+# This just see if the program gets an error and deals with it how you want it.
+try:
+    room_name = current_node['PATHS'][command.upper()]
+    current_node = world_map[room_name]
+except KeyError:
+    print("I can't go that way")
