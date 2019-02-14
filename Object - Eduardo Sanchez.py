4702ebbacf516200b953fb1df661241ad0a44c6b
EddyCSE <44374148+EddyCSE@users.noreply.github.com>
2019-02-14 09:44:40 -0800

    2/14/19

---
+++ Object - Eduardo Sanchez.py
@@ -0,0 +1,29 @@
+class Balloon(object):
+    def __init__(self):
+        self.hole = False
+        self.obtained = True
+        self.water_filled = False
+        self.air_filled = False
+        self.shiny = True
+
+    def fill_with_water(self):
+        if self.air_filled and self.water_filled = False
+            self.water_filled = True
+        else:
+            print("You can't")
+
+    def fill_with_air(self):
+        if self.water_filled = False:
+            self.air_filled = True
+
+    def let_go(self):
+        if self.hole = True:
+            print("You can't, it's broken")
+            return
+        if self.
+
+    def throw(self):
+        if
+
+    def squeak(self):
+        if
