86a54c81633452a230fed27baee476a793a0747a
EddyCSE <44374148+EddyCSE@users.noreply.github.com>
2019-02-21 09:45:47 -0800

    2/21/19

--- Object - Eduardo Sanchez.py
+++ Object - Eduardo Sanchez.py
@@ -5,6 +5,7 @@ class Balloon(object):
         self.water_filled = False
         self.air_filled = False
         self.shiny = True
+        self.normal = True
 
     def fill_with_water(self):
         if self.hole:
@@ -14,6 +15,9 @@ class Balloon(object):
             print("It has already been done.")
         if self.air_filled:
             print("You can't do that.")
+        if self.normal:
+            print("You filled it with water.")
+            self.water_filled = True
         else:
             print("You can't")
 
@@ -25,12 +29,18 @@ class Balloon(object):
             print("You can't do that")
         if self.air_filled:
             print("It has already been done.")
+        if self.normal:
+            print("You filled it with water.")
+            self.air_filled = True
 
     def let_go(self):
         if self.hole:
             print("You can't, it's broken.")
             return
-        if self.water_filled and self.air_filled == False:
+        if self.normal:
+            print("It dropped to the floor.")
+            self.obtained = False
+        if not self.water_filled and self.air_filled:  # False
             print("It dropped to the floor.")
             self.obtained = False
         if self.water_filled:
@@ -45,7 +55,10 @@ class Balloon(object):
         if self.hole:
             print("You can't, it's broken.")
             return
-        if self.water_filled and self.air_filled == False:
+        if self.normal:
+            print("It flew to the floor.")
+            self.obtained = False
+        if not self.water_filled and self.air_filled:  # False
             print("It dropped to the floor.")
             self.obtained = False
         if self.water_filled:
@@ -63,11 +76,13 @@ class Balloon(object):
         if self.shiny:
             print("You squeaked the balloon... great.")
             self.shiny = False
-        if self.shiny == False:
+        if self.normal:
+            print("Nothing happened.")
+        if not self.shiny:  # False
             print("It's not shiny.")
 
     def get_new_balloon(self):
-        if self.obtained == False:
+        if not self.obtained:  # False
             print("You got a new balloon.")
             self.obtained = True
 
@@ -75,11 +90,23 @@ class Balloon(object):
         if self.hole:
             print("You can't, it's broken.")
             return
-        if self.shiny == False:
+        if not self.shiny:  # False
             print("You shined it.")
+# All Definitions:
+# -Fill_with_water
+# -Fill_with_air
+# -Let_go
+# -Throw
+# -Squeak
+# -Get_new_balloon
+# -Shine_balloon
 
 
 my_balloon = Balloon()
 
-my_balloon.fill_with_air()
-my_balloon.shine_balloon()
+my_balloon.let_go()
+my_balloon.get_new_balloon()
+my_balloon.throw()
+my_balloon.get_new_balloon()
+my_balloon.fill_with_water()
+my_balloon.let_go()
