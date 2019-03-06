c4a3cd41b7b0de92884430327c0e987b0b864294
EddyCSE <44374148+EddyCSE@users.noreply.github.com>
2019-03-06 09:45:01 -0800

    3/6/19

--- Eduardo Sanchez - Item.py
+++ Eduardo Sanchez - Item.py
@@ -16,29 +16,61 @@ class Weapon(object):
 
 class Rock(Weapon):
     def __init__(self):
-        super(Rock, self).__init__("Rock", 5)
+        super(Rock, self).__init__("Rock", 5, 10)
 
 
 class MetalBeam(Weapon):
     def __init__(self):
-        super(MetalBeam, self).__init__("Metal_Beam", 20)
+        super(MetalBeam, self).__init__("Metal_Beam", 20, 50)
 
 
 class MakeshiftSword(Weapon):
     def __init__(self):
-        super(MakeshiftSword, self).__init__("MakeshiftSword", 40)
+        super(MakeshiftSword, self).__init__("MakeshiftSword", 40, 10)
 
 
 class Potion(object):
-    def __init__(self, name, heal):
+    def __init__(self, name, heal, shield, amount, health):
         super(Potion, self).__init__(name)
         self.heal = heal
+        self.shield = shield
+        self.amount = amount
+        self.health = health
+
+    def drink_health_potion(self):
+        self.amount -= 1
+        self.health += 50
+
+    def drink_defense_potion(self):
+        self.amount -= 1
+        self.shield += 50
+
+    def drink_strength_potion(self):
+        self.amount -= 1
+        self.health += 0
+
+
+class HealthPotion(Potion):
+    def __init__(self):
+        super(HealthPotion, self).__init__("Health Potion", 50, 0, 2, 0)
+
+
+class ShieldPotion(Potion):
+    def __init__(self):
+        super(ShieldPotion, self).__init__("Shield Potion", 0, 50, 2, 0)
+
+
+class StrengthPotion(Potion):
+    def __init__(self):
+        super(StrengthPotion, self).__init__("Health Potion", 0, 0, 2, 0)
 
 
 class Armor(object):
-    def __init__(self, name, defence):
+    def __init__(self, name, defense):
         super(Armor, self).__init__(name)
-        self.defence = defence
+        self.defense = defense
 
 
-class
+class Helmet(Armor):
+    def __init__(self):
+        super(Helmet, self).__init__("Helmet", 10)
