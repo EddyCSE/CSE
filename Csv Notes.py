05e1a5cde6b9878051b742d919af3f11dc2817e9
EddyCSE <44374148+EddyCSE@users.noreply.github.com>
2019-04-25 09:47:47 -0700

    4/26/19

--- Csv Notes.py
+++ Csv Notes.py
@@ -1,8 +1,29 @@
 import csv
 
-with open("Book1.csv") as old_csv:
-    reader = csv.reader(old_csv)
-    for row in reader:
-        # old_number = int(row[0]) + 1
-        old_number = row[0]
-        print(old_number)
+# with open("Book1.csv", 'r') as old_csv:
+#     with open("MyNewFile.csv", 'w', newline='') as new_csv:
+#         print("Writing file...")
+#         reader = csv.reader(old_csv)
+#         writer = csv.writer(new_csv)
+#         for row in reader:
+#             old_number = int(row[0])
+#             new_number = old_number + 1
+#             row[0] = new_number
+#             writer.writerow(row)
+#             # old_number = int(row[0]) + 1
+#             # print(old_number)
+# print("OK")
+
+with open("Book1.csv", 'r') as old_csv:
+    with open("MyNewFile.csv", 'w', newline='') as new_csv:
+        print("Writing file...")
+        reader = csv.reader(old_csv)
+        writer = csv.writer(new_csv)
+        for row in reader:
+            old_number = row[0]  # This is a string
+            first_num = int(old_number[0])  # This is the first number
+            if first_num % 2 == 0:
+                writer.writerow(row)
+            # old_number = int(row[0]) + 1
+            # print(old_number)
+print("OK")
