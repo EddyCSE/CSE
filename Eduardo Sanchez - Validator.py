657bc5c7915b7f8c5cff71c563b858b0ec10a25d
EddyCSE <44374148+EddyCSE@users.noreply.github.com>
2019-05-02 09:44:15 -0700

    5/2/19

--- Eduardo Sanchez - Validator.py
+++ Eduardo Sanchez - Validator.py
@@ -1,25 +1,20 @@
 # Does it gave 16 digits - Y
-# Drop the last digit from the number. The last digit is what we want to check against
-# Reverse the numbers
+# Drop the last digit from the number. The last digit is what we want to check against - Y
+# Reverse the numbers - Y
 # Multiply the digits in odd positions (1, 3, 5, etc.) by 2 and subtract 9 to all any result higher than 9
 # Add all the numbers together
 # The check digit (the last number of the card) is the amount that you would need to add to get a multiple of 10
 
-
-def reverse_it(string):
-    print(string[::-1])
+Taken_out_word = []
 
 
-reverse_it(num)
-
-
-def take_last_digit(num: str):
-    list_num = list(num)
-    for index in range(len(list_num)):
-        list_num[index] = int(list_num[index])
-    last_digit = list_num[15]
-    list_num.pop(15)
-    print("Last digit = Y")
+def v(num: str):
+    print(num)
+    if not has_16_digits(num):
+        return False
+    take_last_digit(num)
+    reverse_it(num)
+    print(Taken_out_word)
 
 
 def has_16_digits(num: str):
@@ -28,10 +23,23 @@ def has_16_digits(num: str):
         return True
 
 
-def v(num: str):
-    if not has_16_digits(num):
-        return False
-    take_last_digit(num)
+def take_last_digit(num: str):
+    list_num = list(num)
+    Taken_out_word.append(list_num[15])
+    for index in range(len(list_num)):
+        list_num[index] = int(list_num[index])
+    list_num.pop(15)
+    print(list_num)
+    print("Last digit = Y")
+
+
+def reverse_it(list_num: str):
+    list_num = (list_num[::-1])
+    print(list_num)
+    print("Reversing = Y")
+
+
+# def multiply_odds(list_num: str):
 
 
 print(v("5431709304959590"))
