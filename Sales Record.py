f541dbccc077585875b0c5a92826e7f86ac9f3e3
EddyCSE <44374148+EddyCSE@users.noreply.github.com>
2019-05-08 09:47:24 -0700

    5/8/19

--- Sales Record.py
+++ Sales Record.py
@@ -1,1 +1,10 @@
 import csv
+
+
+with open("Sales Records.csv", 'r') as o_csv:
+    reader = csv.reader(o_csv)
+    for row in reader:
+        row = row[13]
+        print(row)
+
+# with open("MyNewFile.csv", 'w', newline='') as new_csv:
