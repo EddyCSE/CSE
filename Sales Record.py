f2d17aede90d9d16307a4974b1224f7a3f9f3a31
EddyCSE <44374148+EddyCSE@users.noreply.github.com>
2019-05-10 10:41:09 -0700

    5/10/19

--- Sales Record.py
+++ Sales Record.py
@@ -1,9 +1,13 @@
 import csv
+
+Total = 0
+
 with open("Sales Records.csv", 'r') as o_csv:
     reader = csv.reader(o_csv)
-    for column in reader:
-        column = column[13]
-        print(column)
-
-# roww = row[2]
-# if roww = ("Snacks"):
+    for row in reader:
+        if row[2] == "Snacks":
+            Total += int(row[13])
+            print(row[2])
+            print(row[13])
+            print()
+print(Total)
