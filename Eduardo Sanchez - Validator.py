68db3d93a2af002a6acb91f642decfc512eaa779
EddyCSE <44374148+EddyCSE@users.noreply.github.com>
2019-05-21 09:45:41 -0700

    5/21/19

--- Eduardo Sanchez - Validator.py
+++ Eduardo Sanchez - Validator.py
@@ -1,20 +1,21 @@
-# Does it gave 16 digits - Y
-# Drop the last digit from the number. The last digit is what we want to check against - Y
-# Reverse the numbers - Y
-# Multiply the digits in odd positions (1, 3, 5, etc.) by 2 and subtract 9 to all any result higher than 9
-# Add all the numbers together
-# The check digit (the last number of the card) is the amount that you would need to add to get a multiple of 10
+# 1. Does it gave 16 digits - Working
+# 2. Drop the last digit from the number. The last digit is what we want to check against - Working
+# 3. Reverse the numbers - Working
+# 4. Multiply the digits in odd positions by 2 and subtract 9 to all any result higher than 9 - Not Working
+# 5. Add all the numbers together - Not Working
+# 6. The taken out num is the amount that you would need to add to get a multiple of 10 - Not Working
 
 Taken_out_num = []
 
 
 def v(num: str):
+    print("------------------------------")
     print(num)
     if not has_16_digits(num):
         return False
     new_list = take_last_digit(num)
     reverse_it(new_list)
-    # multiply_odds(new_list)
+    # multiply_odds(new_new_list)
 
 
 def has_16_digits(num: str):                    # Works
@@ -47,24 +48,22 @@ def reverse_it(list_num: str):                  # Works
     return list_num
 
 
-# def multiply_odds(new_num: str):
-#     if not div_by_2(new_num):
-#         odd_num = new_num * 2
-#         if odd_num > 9:
-#             odd_num -= 9
-#         else:
-#             pass
-#             print(odd_num)
-#             return odd_num
+def multiply_odds(new_num: str):         # Not working
+    for i in (len(new_num)):
+        if not div_by_2(new_num):
+            i = i * 2
+            if new_num > 9:
+                new_num -= 9
+            return len(new_num)
 
 
-def div_by_2(list_num: str):
+def div_by_2(list_num: str):              # Not working
     if list_num % 2 == 0:
         return True
     return False
 
 
-# pre_odd_num = int(list_num)
+# pre_odd_num = int(list_num)             # Not Working
 #      if pre_odd_num == [1, 3, 5, 7, 9]:
 #          odd_num = pre_odd_num * 2
 #          if odd_num == [9, 10, 11, 12, 13, 14, 15, 16, 17, 18]:
@@ -74,5 +73,6 @@ def div_by_2(list_num: str):
 #      else:
 #          pass
 
-
-print(v("5431709304959590"))
+print("------------------------------")
+numb = input("Enter a 16 digit credit card number here. --->")
+print(v(numb))
